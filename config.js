9dfa43e1696e0472647d92a8b7691c3a36f2866e
angeliwanov <angel.iwanov@gmail.com>
2022-12-09 08:27:32 +0200

    eventListener scroll

---
+++ config.js
@@ -0,0 +1,4 @@
+const config = {
+    MY_KEY : 'w952MQ39Ca4US1WoPMAlRLoUBx9p6l7I-K4CGeg-rDo',
+    SECRET_KEY : 'A-aGxRQtEXXA7fy6lYp4lRj5DSYzikFLfoAq7fe9T8A'
+}
--- script.js
+++ script.js
@@ -2,12 +2,29 @@ const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
 let photosArray = [];
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
 
 // Unsplash Api
-const count = 10;
+const count = 5;
 const mykey = config.MY_KEY;
 const secretkey = config.SECRET_KEY;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${mykey}&count=${count}`;
+
+// Check if all images are loaded
+function imageLoaded() {
+    // console.log(imagesLoaded)
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        count = 30;
+        apiUrl = `https://api.unsplash.com/photos/random/?client_id=${mykey}&count=${count}`;
+        // console.log('ready = ', ready)
+    }
+}
 
 // helper function to set attributes 
 function setAttributes(element, attributes) {
@@ -18,6 +35,9 @@ function setAttributes(element, attributes) {
 
 // Create elements for photos, add to DOM
 function displayPhotos () {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    // console.log('total images ', totalImages)
     photosArray.forEach((photo) => {
          // create <a> to link to unsplash
         const item = document.createElement('a');
@@ -32,6 +52,8 @@ function displayPhotos () {
             alt: photo.alt_description,
             title: photo.alt_description
         })
+        // event listener when image is finished loading
+        img.addEventListener('load', imageLoaded);
         // put <img> inside <a>, put both inside imageContainer
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -50,6 +72,14 @@ async function getPhotos() {
         console.log(error)
     }
 }
+
+// Load more photos on scrolling to the bottom
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+         ready = false;
+         getPhotos()
+    }
+})
 
 // Load
 getPhotos();
